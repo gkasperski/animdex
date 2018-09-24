002594047a8ee4eab2dac1c37b88a05e7060bcfb
Grzegorz Kasperski <grzegorz.kasperski@globallogic.com>
2018-09-24 15:58:55 +0200

    Just linting

--- app/components/Scenarios/Scenarios.js
+++ app/components/Scenarios/Scenarios.js
@@ -1,7 +1,5 @@
 import React from 'react';
 
-export const Scenarios = () => (
-  <div>
-    Test
-  </div>
-)
+const Scenarios = () => <div>Test</div>;
+
+export default Scenarios;
--- app/middlewares/ipc.middleware.js
+++ app/middlewares/ipc.middleware.js
@@ -1,16 +1,16 @@
-import {ipcRenderer} from 'electron'
+import { ipcRenderer } from 'electron';
 
 ipcRenderer.on('eventsList', (event, message) => {
   // Read message list
-  // Set up actions 
-  console.log('event',event)
-  console.log('message',message)
-  this.setState({'events': message})
-})
+  // Set up actions
+  console.log('event', event);
+  console.log('message', message);
+  this.setState({ events: message });
+});
 
-export default store => next => (action) => {
-  if(action.type.startsWith('IPC/')){
+export default store => next => action => {
+  if (action.type.startsWith('IPC/')) {
     // Handle ipc actions
   }
-  next(action)
-}
+  next(action);
+};
